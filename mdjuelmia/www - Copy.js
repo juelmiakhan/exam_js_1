efb003883cd6cc3156430e8b568846974e5be624
juelmiakhan <148678603+juelmiakhan@users.noreply.github.com>
2023-10-22 13:49:38 +0600

    Add files via upload

---
+++ mdjuelmia/www - Copy.js
@@ -0,0 +1,55 @@
+// 7.
+// var %, **, ++, --, /
+// %  = er kaj holo kono songkhar koto persent hobe ta boja = jemn  100takai 1taka
+// ** = er mane kono doiti songkha ke gon kora =jemn 2**2 =4 taka
+// ++ = er mane holo jekono doiti songkah ke jok kora jemn =2++2=4 taka
+// -- =er mane holo jekono doiti songkah ke ek songkah ke onno songkah theke biok kora jemn 5--3=2
+// / = er mane holo jekono songkah ke onno ekti songkha ke bag kora jemn =10/2= 5
+// 2.
+// variable dickleare korar jonno je je step folo korte hoi ta holo = prothome ekti var nite hbe tarpor bisoi ebong jok na biyok hbe ta likte hbe 
+// tarpor console log kore result dekhte hbe
+// 11.
+// var =8;
+// var 8**3;
+// console.log(total)
+// 13.
+// var x=30;
+// var y=23;
+// var  total =x+y;
+// console.log(total)
+// 30.
+// somoi er best use kora manob jibone khobi prioyojon . ja sob manus sothik korte pare na .somoi khobi molloban ekti sompod .
+// somoi ke best use kora jai rotine kore . jemn doren ami kon somoi gom theke utbo tar ekta rotine kora .
+// abr kokhn khabar khabo koi min er ekta rotine kora . 
+// kon kaj kotokhn korbo tar ekta rotine kora . 
+// abr kotokha rest nibo tar ekta rotine kora . 
+// abr ami sara din ki ki kaj korbo kotokhon korbo tar ekta poparlyy rotine kora jate somoi er sothik bebohar kora jai . 
+25. 
+// var total kolom =10;
+// var total kolomer dam=185;
+ 
+// var total kolomer dam /total kolom;
+
+// console.log(ekpice kolomer dam)
+
+6.
+// arithmetic operator er kaj holo ganitik somosae somadan kora.
+16.
+// arre er index suro hoi 0 theke .
+17.
+// var number=[34,56,74,24,88,34,776,34,5];
+// var  number= ? {5};
+//  console.log(5)
+ 20.
+//  var team1= ["rahim","karim"] ;
+//  var team2=["tanim","biplo","tareq"];
+//  var team3=["sajjad", "arefin",];
+//  var team1+team2+team3;
+//  console.log(total result);
+21.
+// var fruits=["banana","orange","apple","mango"];
+// var fruits=? {1};
+// console.log(result );
+//  3.
+// e bolte bojai je ba bibenno data typngla, english diye type kore je name ba data likha hoi tai data type . jemn=juel ,tamim,shamim .
+
